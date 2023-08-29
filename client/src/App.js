7da92402520b5fdd03d4c6731103e231e146f798
jazibjavaid <javaid.j@northeastern.edu>
2023-08-29 18:03:50 -0400

    Added login/signup form

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="App">
@@ -18,9 +19,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline/> 
         <Routes>
-          <Route path = "/home" element = {<HomePage />}/>
+          <Route path = "/home" element = {isAuth ? <HomePage /> : <Navigate to = "/" />}/>
           <Route path = "/" element = {<LoginPage />}/>
-          <Route path = "/profile/:userId" element = {<ProfilePage />}/>
+          <Route path = "/profile/:userId" element = {isAuth ? <ProfilePage /> : <Navigate to = "/" />}/>
         </Routes>
       </ThemeProvider>
       </BrowserRouter>
