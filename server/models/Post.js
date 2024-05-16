8864073c6f9f612459632b12eb0f0f2dc5ccc281
jazibjavaid <javaid.j@northeastern.edu>
2024-05-15 23:43:55 -0400

    Bug fix in server code

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema(
     {
+        userId: {
+            type: String,
+            required: true,
+        },
         firstName: {
             type: String,
             required: true,
